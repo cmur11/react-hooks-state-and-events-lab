2d9333ef790b7ce54a1abc655ace84a5e4b6562b
Conor Murnane <conormurnane11@gmail.com>
2021-02-03 08:48:11 -0500

    completed w solution help

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+console.log(itemData)
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [light, setDark] = useState(true)
+  
+  // console.log(light)
 
+  function toggleSwitch(){
+    setDark(!light)
+  }
   // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
-
-  return (
-    <div className={"App " + (false ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
-  );
+   console.log(items);
+    return (
+      <div className={"App " + (light ? "dark" : "light")}>
+        {/* was false ^^ instead of light ? */}
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={toggleSwitch}>
+            {light ? "dark" : "light"}</button>
+          {/* why does ^ this work */}
+        </header>
+        <ShoppingList items={items} />
+      </div>
+    );
+    
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [click,setClick] = useState(false)
+
+  function handleClick(e){
+  
+    setClick(!click)
+  }
+console.log(click)
   return (
-    <li className="">
+    <li className={click ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick = {handleClick} className="add">{click ? "Remove From" : "Add to Cart"}</button>
     </li>
   );
 }
